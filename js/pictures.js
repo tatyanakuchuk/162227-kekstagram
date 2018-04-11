83c57e799b884af2fbd449147b8f2a390477d784
Tatyana <tatyana8787@gmail.com>
2018-04-11 17:59:58 +0200

    3е задание

--- js/pictures.js
+++ js/pictures.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var photoComments = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
@@ -27,25 +29,18 @@ var findRandomInt = function (min, max) {
 var getRandomIndex = function (arr) {
   var randomIndex = Math.floor(Math.random() * arr.length);
   return arr[randomIndex];
-}
-
-
+};
 // Объект описание фотографии
 var userPhoto = {
-  url: function() {
-      var image = document.createElement('img');
-      image.src = 'photos/' + i + '.jpg';
-      return image.src;
+  url:function (i) {
+    this.src = 'photos/' + i + '.jpg';
+    return this.src;
   },
-  likes: function () {
-    var minLikes = 15;
-    var maxLikes = 200;
+  likes: function (minLikes, maxLikes) {
     var likes = findRandomInt(minLikes, maxLikes);
     return likes;
   },
-  comments: function () {
-    var minСomments = 1;
-    var maxСomments = 2;
+  comments: function (minСomments, maxСomments) {
     var numberComments = findRandomInt(minСomments, maxСomments);
     return numberComments;
   },
@@ -56,32 +51,33 @@ var userPhoto = {
 
 var allPictures = document.querySelector('.pictures');
 var template = document.querySelector('#picture').content.querySelector('.picture__link');
-// Зачем создаем, если уже есть один на странице???
 var fragment = document.createDocumentFragment();
 
-var array = [];
-for (var i = 1; i <= 25; i++) {
-  var newPhoto = template.cloneNode(true);
-  newPhoto.querySelector('img').src = userPhoto.url();
-  newPhoto.querySelector('.picture__stat--likes').textContent = userPhoto.likes();
-  newPhoto.querySelector('.picture__stat--comments').textContent = userPhoto.comments();
-  array.push(newPhoto);
-  fragment.appendChild(newPhoto);
-}
-allPictures.appendChild(fragment);
 
+var newElement = function () {
+  var newPhoto = template.cloneNode(true);
+  newPhoto.querySelector('img').src = userPhoto.url(i);
+  newPhoto.querySelector('.picture__stat--likes').textContent = userPhoto.likes(15, 200);
+  newPhoto.querySelector('.picture__stat--comments').textContent = userPhoto.comments(1, 2);
+  fragment.appendChild(newPhoto);
+  allPictures.appendChild(fragment);
+}
+
+for (var i = 1; i <= 25; i++) {
+  newElement();
+}
 
 // Большая фотка
 var bigPicture = document.querySelector('.big-picture');
 
 var getBigPhoto = function () {
-  bigPicture.classList.remove('hidden');
-  bigPicture.querySelector('img').src = userPhoto.url(i=1);
-  bigPicture.querySelector('.likes-count').textContent = userPhoto.likes(i=1);
-  bigPicture.querySelector('.comments-count').textContent = userPhoto.comments(i=1);
+  bigPicture.querySelector('img').src = userPhoto.url(1);
+  bigPicture.querySelector('.likes-count').textContent = userPhoto.likes(15, 200);
+  bigPicture.querySelector('.comments-count').textContent = userPhoto.comments();
   bigPicture.querySelector('.social__comments').innerHTML = '<li class="social__comment social__comment--text"><img class="social__picture" src="img/avatar-' + findRandomInt(1, 6) + '.svg" alt="Аватар комментатора фотографии" width="35" height="35">' + getRandomIndex(photoComments) + '</li>';
   document.querySelector('.social__comment-count').classList.add('visually-hidden');
   document.querySelector('.social__comment-loadmore').classList.add('visually-hidden');
   document.querySelector('.social__comments li').style.maxWidth = '600px';
-}
+  bigPicture.classList.remove('hidden');
+};
 getBigPhoto();
