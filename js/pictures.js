4149d01af0bfade3f026fe5e708e6bb6e53745ef
Tatyana <tatyana8787@gmail.com>
2018-04-23 21:58:33 +0200

    Показ изображения в полноэкранном режиме

--- js/pictures.js
+++ js/pictures.js
@@ -71,10 +71,40 @@ var getBigPhoto = function () {
   document.querySelector('.social__comment-count').classList.add('visually-hidden');
   document.querySelector('.social__comment-loadmore').classList.add('visually-hidden');
   document.querySelector('.social__comments li').style.maxWidth = '600px';
-  bigPicture.classList.remove('hidden');
+  openBigPhoto();
 };
 // getBigPhoto();
 
+var bigPhotoEscPressHandler = function (evt) {
+  if (evt.keyCode === 27) {
+    closeBigPhoto();
+  }
+};
+var openBigPhoto = function () {
+  bigPicture.classList.remove('hidden');
+  document.addEventListener('keydown', bigPhotoEscPressHandler);
+  document.querySelector('body').classList.add('modal-open');
+};
+var closeBigPhoto = function () {
+  bigPicture.classList.add('hidden');
+  document.removeEventListener('keydown', bigPhotoEscPressHandler);
+  document.querySelector('body').classList.remove('modal-open');
+};
+
+var bigPhotoClose = document.querySelector('.big-picture__cancel');
+  bigPhotoClose.addEventListener('click', function () {
+    closeBigPhoto();
+  });
+
+
+// Показ изображения в полноэкранном режиме
+var showBigPhoto = function () {
+  var thumbsPhotos = document.querySelectorAll('.picture__img');
+  for (var i = 0; i < thumbsPhotos.length; i++) {
+    thumbsPhotos[i].addEventListener('click', getBigPhoto);
+  }
+};
+showBigPhoto();
 
 // ДОБАВЛЕНИЕ ФОТО
 var uploadFile = document.querySelector('.img-upload__input');
@@ -234,17 +264,17 @@ radioArea.addEventListener('change', radioChangeHandler);
 // ХЭШ-ТЕГИ
 var hashtagsInput = document.querySelector('.text__hashtags');
 var hashtagsInputHandler = function () {
-  var regexp = /^#\S+$/i;
-  if ( /^#\S+$/i.test(hashtagsInput.value) ) {
+  var regexp = /^#\S{20}$/i;
+  if (/^#\S{20}$/i.test(hashtagsInput.value)) {
     console.log(1);
   }
   else {
     console.log(2);
   }
-
+  var hashtagsStr = hashtagsInput.value;
 };
 
-hashtagsInput.addEventListener('change', hashtagsInputHandler);
+hashtagsInput.addEventListener('keyup', hashtagsInputHandler);
 
 hashtagsInput.addEventListener('focus', function () {
   document.removeEventListener('keydown', popupEscPressHandler);
