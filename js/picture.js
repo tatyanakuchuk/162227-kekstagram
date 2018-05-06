81d0bbf18eba3bb9a02818a67cc62bb8edf6a8d4
Tatyana <tatyana8787@gmail.com>
2018-05-06 23:24:07 +0200

    исправления

--- js/picture.js
+++ js/picture.js
@@ -20,6 +20,10 @@
   renderAllPictures();
   var bigPicture = document.querySelector('.big-picture');
 
+  var getRandomComment = function () {
+    return '<li class="social__comment social__comment--text"><img class="social__picture" src="img/avatar-' + window.util.findRandomInt(1, 6) + '.svg" alt="Аватар комментатора фотографии" width="35" height="35">' + window.util.getRandomIndex(window.data.photoComments) + '</li>';
+  };
+
   var writeComments = function (min, max) {
     var comment = '';
     for (var i = 1; i <= window.util.findRandomInt(min, max); i++) {
@@ -40,10 +44,19 @@
     openBigPhoto();
   };
 
+  var ESC_KEYCODE = 27;
+
+  var bigPhotoEscPressHandler = function (evt) {
+    if (evt.keyCode === ESC_KEYCODE) {
+      closeBigPhoto();
+    }
+  };
+
   var openBigPhoto = function () {
     bigPicture.classList.remove('hidden');
     document.addEventListener('keydown', bigPhotoEscPressHandler);
     document.querySelector('body').classList.add('modal-open');
+    showBigPhoto();
   };
 
   var showBigPhoto = function () {
